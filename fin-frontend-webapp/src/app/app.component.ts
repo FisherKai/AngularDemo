5ea9f51d97ffc742947984ddbb50fc7100dd11e8
yukai <1079869931@qq.com>
2021-04-21 19:14:36 +0800

    update

--- fin-frontend-webapp/src/app/app.component.ts
+++ fin-frontend-webapp/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  constructor(){
+    console.log('constructor');
+  }
+
   title = 'fin-frontend-webapp';
+
+  ngOnInit(){
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(){
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked ');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit ');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked ');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy ');
+  }
 }
